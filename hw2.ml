d1b801488f0ce6696cbd62e2ac00648ee9e46fb1
Pohvalister <pohvalister@mail.ru>
2017-09-14 00:11:03 +0300

    hw2 - some fuctions added

--- hw2.ml
+++ hw2.ml
@@ -1,16 +1,44 @@
 open Hw1;;
 
-type lambda = Hw1.lambda;;
-let alpha_equ_of x y =
-	let rec alpha_equ_supp lam1 lam2 lisx = 
-		match (lam1,lam2) with
-			(Var v1, Var v2) -> v1=v2 || (List.exists (fun (z, w) -> (z=v1 && w =v2)) lisx)
-			|(App (l1, l2), App (l3,l4)) -> ((alpha_equ_supp l1 l3 lisx) && (alpha_equ_supp l2 l4 lisx))
-			|(Abs (var1, sen1), Abs (var2, sen2)) -> alpha_equ_supp sen1 sen2 ((var1,var2)::lisx)
-			|_ -> false in
-	alpha_equ_supp x y [];;
+module StringMap = Map.Make (
+struct
+	type t = string
+	let compare = String.compare
+end);;
 
-let free_to_add th lam x = 
+module StringSet = Set.Make (
+struct
+	type t = string
+	let compare = String.compare
+end);;
+
+type lambda = Hw1.lambda;;
+
+let free_vars lam =
+	let rec free_vars_rec lam = 
+		match lam with 
+		Var x  ->  StringSet.add x StringSet.empty 
+		| App (lam1, lam2) -> StringSet.union (free_vars_rec lam1) (free_vars_rec lam2)
+		| Abs (var, lam) -> StringSet.remove var (free_vars_rec lam)
+	in
+	StringSet.elements (free_vars_rec lam)
+;;
+
+let free_to_subst rePl lam var =
+	let rePl_vars = StringSet.of_list (free_vars rePl) in
+
+	(*checking from bottom: 1 - exists, but OK; 0 - doesn't exists; -1 - exists and broken *)
+	let rec is_free lam=
+		match lam with
+		Var place -> if (place = var) then 1 else 0
+		| App(lam1, lam2) -> let tmp1, tmp2 = (is_free lam1), (is_free lam2) in (if ((tmp1 = -1)|| (tmp2 = -1))	then -1
+																											else if (tmp1 = 1 || tmp2 = 1)	then 1
+																																			else 0)
+		| Abs(var1, lam) -> let tmp = is_free(lam) in if (var1=var || tmp=0) then 0 else if (tmp = -1 ||(tmp=1 && StringSet.mem var1 rePl_vars)) then -1 else 1
+	in
+	(is_free lam) <> -1
+;;
+(*let free_to_subst th lam x = 
 	let rec get_free_vars l fixed free = 
 		let rec my_merge l1 l2 = match l1 with [] -> l2|h::t -> my_merge t (h::l2) in
 
@@ -26,3 +54,49 @@ let free_to_add th lam x =
 		|App (l1, l2) -> (check_fixed_in l1 point fixed vars) || (check_fixed_in l2 point fixed vars)
 		|Abs (vStr, l1) -> check_fixed_in l1 point (vStr::fixed) vars in
 	not (check_fixed_in lam x [] (get_free_vars th [] []));;
+*)
+
+
+let is_alpha_equivalent lam1 lam2 = 
+	let uniqueVar = ref "" in
+		
+	let rec is_alpha_equivalent_rec lam1 map1 lam2 map2 =
+		let renew_unique = 
+			uniqueVar := !uniqueVar ^ "0";
+			!uniqueVar
+		in
+
+		let evolve var map =
+			if (StringMap.mem var map)	then (StringMap.find var map)
+										else var
+		in
+
+		let update map oldVar newVar =
+			if (StringMap.mem oldVar map)	then map
+											else StringMap.add oldVar newVar map
+		in
+
+		match (lam1,lam2) with
+		(Var var1, Var var2) -> ((evolve var1 map1) = (evolve var2 map2))
+		|(App (lam11, lam12), App(lam21,lam22)) -> ((is_alpha_equivalent_rec lam11 map1 lam21 map2) && (is_alpha_equivalent_rec lam12 map1 lam22 map2))
+		|(Abs (var1, lam1), Abs (var2, lam2)) -> let change = renew_unique in is_alpha_equivalent_rec lam1 (update map1 var1 change) lam2 (update map2 var2 change)
+		| _ -> false
+	in
+
+	uniqueVar:= (Hw1.string_of_lambda lam1)^(Hw1.string_of_lambda lam2);
+	is_alpha_equivalent_rec lam1 StringMap.empty lam2 StringMap.empty
+;;
+
+
+let is_normal_form lam =
+	let rec is_normal_form_rec lam =
+		match lam with
+			Var v -> true
+			|App (Abs (var,lam1),lam2) -> false
+			|App (lam1, lam2) -> (is_normal_form_rec lam1) && (is_normal_form_rec lam2)
+			|Abs (var, lam) -> is_normal_form_rec lam in
+	is_normal_form_rec lam
+
+;;
+
+
--- t.ml
+++ t.ml
@@ -24,15 +24,16 @@ print_int (int_of_peano (div (peano_of_int 7) (peano_of_int 2))); print_string "
 print_int (int_of_peano (power (peano_of_int 2) (peano_of_int 3))); print_string "\n";;
 print_int (int_of_peano (power (peano_of_int 2) (peano_of_int 0))); print_string "\n";;
 
+print_string "\narithmetic_OK\n";;
 
-let rec print_list = function 
+let rec print_list_int = function 
 [] -> ()
-| h::t -> print_int h ; print_string " " ; print_list t;;
+| h::t -> print_int h ; print_string " " ; print_list_int t;;
 
 (* reverse*)
 let l = [1;2;3;4;5;6;7;8;9;10];;
-print_list l;;
-print_list (rev l);;
+print_list_int l;;
+print_list_int (rev l);;
 
 (*merge sort*)
 let l1 = [4;3;2;1];;
@@ -41,9 +42,11 @@ let l3 = [909;66;-33;22;8;1;4;4;2;0];;
 
 
 
-print_list (merge_sort l1);;
-print_list (merge_sort l2);;
-print_list (merge_sort l3);;
+print_list_int (merge_sort l1);;
+print_list_int (merge_sort l2);;
+print_list_int (merge_sort l3);;
+
+print_string "\nlist_OK\n";;
 
 (*lambda parsers *)
 print_string (string_of_lambda (lambda_of_string "\\x.\\y.x"));;
@@ -52,26 +55,46 @@ print_string (string_of_lambda (lambda_of_string "(\\y.x) z"));;
 print_string (string_of_lambda (lambda_of_string "z x \\y.(y r)"));;
 print_string (string_of_lambda (lambda_of_string "\\aaaaaaa.(xxxxxx dsfdfd)"));;
 print_string (string_of_lambda (lambda_of_string "((x)) (((((y)))))"));;
-print_string (string_of_lambda (lambda_of_string "(("));;
+(*print_string (string_of_lambda (lambda_of_string "(("));;*)
 let x = App(Var "ad", Abs ("as", Var ("ad")));;
 print_string (string_of_lambda x);;
 
+print_string "\nparsings_OK\n";;
+
 (*lambda alpha_equ*)
 let test st1 st2 b = 
-	print_string (string_of_bool (b = (alpha_equ_of (lambda_of_string st1) (lambda_of_string st2))));;
+	print_string (string_of_bool (b = (is_alpha_equivalent (lambda_of_string st1) (lambda_of_string st2))));;
 
 test "(x)" "(y)" false;;
 test "x y" "x y" true;;
-test "\\x.x y" "\\y.y y";;
+test "\\x.x y" "\\y.y y" false;;
 test "(\\x.x)(z)(w)" "(\\y.y)(z)(w)" true;;
 test "\\x1.\\x2.\\x3.\\x4.x1 x2 x3 x4" "\\y1.\\y2.\\y3.\\y4.y1 y2 y3 y4" true;;
 test "\\x1.\\x2.\\x3.\\x4.(x1 x2 x3 x4)" "\\y1.\\y2.\\y3.\\y4.(y1 y2 y3 y4)" true;;
 test "\\y1.\\y2.\\y3.\\y4.y1 y2 y3 y4" "\\x1.\\x2.\\x3.\\x4.(x4 x2 x3 x1)" false;;
 test "\\x1.\\x2.x1 x2" "\\y1.\\y2.y2 y1" false;;
 
+print_string "\nalpha_equ_OK\n";;
+
+let rec print_list_string = function 
+[] -> ()
+| h::t -> print_string h ; print_string " " ; print_list_string t;;
+
+(* free_vars*)
+print_list_string ( free_vars (lambda_of_string "\\x.\\y.x"));;
+print_list_string ( free_vars (lambda_of_string "y x"));;
+print_list_string ( free_vars (lambda_of_string "(\\y.x) z"));;
+print_list_string ( free_vars (lambda_of_string "z x \\y.(y r)"));;
+print_list_string ( free_vars (lambda_of_string "\\aaaaaaa.(xxxxxx dsfdfd)"));;
+print_list_string ( free_vars (lambda_of_string "((x)) (((((y)))))"));;
+
+
+
+print_string "\nfree_vars_OK\n";;
+
 (*free_subst*)
 let test st1 st2 diff b =
-	print_string (string_of_bool (b = (free_to_add (lambda_of_string st1) (lambda_of_string st2) diff)));;
+	print_string (string_of_bool (b = (free_to_subst (lambda_of_string st1) (lambda_of_string st2) diff)));;
 
 test "x" "\\x.y" "y" false;;
 test "x" "\\x.x" "y" true;;
@@ -82,3 +105,11 @@ test "x y \\z.z" "\\x.b" "a" true;;
 test "x y \\z.z" "\\x.b a" "a" false;;
 test "x y \\z.z" "(\\x.b) a" "a" true;;
 
+print_string "\nsubst_OK\n";;
+
+(*norm_form*)
+let test st1 b =
+	print_string (string_of_bool (b = (is_normal_form (lambda_of_string st1))));;
+
+test "\\x.x y" true;;
+test "(\\x.x) y" false;;
